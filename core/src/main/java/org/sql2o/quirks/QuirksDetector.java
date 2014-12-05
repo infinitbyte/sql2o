0256752edae21a0318d6ee1a96809c2f73f932ac
medcl <m@medcl.net>
2014-12-05 17:29:37 +0800

    fix #159

--- core/src/main/java/org/sql2o/quirks/QuirksDetector.java
+++ core/src/main/java/org/sql2o/quirks/QuirksDetector.java
@@ -29,10 +29,17 @@ public class QuirksDetector{
         if(jdbcObject instanceof GenericDatasource)
             return forURL(((GenericDatasource) jdbcObject).getUrl());
         Quirks quirks;
-        for (QuirksProvider provider : providers) {
-            quirks=provider.forObject(jdbcObject);
-            if(quirks!=null) return quirks;
+
+        if(providers!=null){
+            Iterator<QuirksProvider> itr = providers.iterator();
+            while (itr.hasNext()) {
+                QuirksProvider next = itr.next();
+                quirks=next.forObject(jdbcObject);
+                if(quirks!=null) return quirks;
         }
-        return new NoQuirks();
+    }
+
+    return new NoQuirks();
+
     }
 }
